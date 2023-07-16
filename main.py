24af7f14cea350ced878ca4d223fc2f56d306814
KAPIL INANIA <67285213+kapilinania@users.noreply.github.com>
2023-07-16 20:35:51 +0530

    robo almost done

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import pyttsx3
 if __name__ == '__main__':
-    print("Welcome to Robo Speaker:")
-    x = input("Enter what you want me to speak: ")
+    while True:
+        print("Welcome to Robo Speaker: Create By Kapil Inania")
+        x = input("Enter what you want me to speak: ")
+        if x == "q":
+            break
 
-    # Initialize the pyttsx3 engine
-    engine = pyttsx3.init()
+        # Initialize the pyttsx3 engine
+        engine = pyttsx3.init()
 
-    # Get the current speech rate
-    current_rate = engine.getProperty('rate')
+        # Get the current speech rate
+        current_rate = engine.getProperty('rate')
 
-    # Set a slower speech rate (adjust the value as needed)
-    engine.setProperty('rate', current_rate - 100)
+        # Set a slower speech rate (adjust the value as needed)
+        engine.setProperty('rate', current_rate - 100)
 
-    # Speak the provided text
-    engine.say(x)
+        # Speak the provided text
+        engine.say(x)
 
-    # Wait until speech is complete
-    engine.runAndWait()
+        # Wait until speech is complete
+        engine.runAndWait()
